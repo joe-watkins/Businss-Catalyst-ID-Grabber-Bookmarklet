32eedd04afbaa28bda1f561be1dd199b85dd4070
Joe Watkins <info@joe-watkins.io>
2014-02-28 08:03:48 -0800

    js cleanup - added .min

--- bc-id-grabber-bookmarklet.js
+++ bc-id-grabber-bookmarklet.js
@@ -1,15 +1,17 @@
-javascript: jQuery("div.RadGrid_Default,div.RadTreeView_Default").each(function() {
-    jQuery(this).find("a").each(function() {
-        var link = jQuery(this).attr("href");
-        var text = jQuery(this).text();
-        var curURL = window.location.href;
-        link = link.split("=");
+javascript: $("div.RadGrid_Default,div.RadTreeView_Default").each(function() {
+    $(this).find("a").each(function() {
+        var link = $(this).attr("href").split("="),
+        text = $(this).text(),
+        curURL = window.location.href;
         if (curURL.indexOf("CustomContent_ItemList.aspx") != -1) {
-            jQuery(this).text(text + " - " + link[2].replace("&ValueID", ""));
+            $(this).text(text + " - " + link[2].replace("&ValueID", ""));
         } else if (curURL.indexOf("WebForms.aspx") != -1) {
-            jQuery(this).text(text + " - " + link[1]);
+            $(this).text(text + " - " + link[1]);
         } else {
-            jQuery(this).text(text + " - " + link[1].replace("&A", "").replace("&CustomContent", "").replace("WebFormID", "").replace("&ValueID", ""));
+            $(this).text(text + " - " + link[1].replace("&A", "")
+                .replace("&CustomContent", "")
+                .replace("WebFormID", "")
+                .replace("&ValueID", ""));
         }
     });
 });
