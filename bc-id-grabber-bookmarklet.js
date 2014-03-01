5866cd6768ea8063bd75c031f44803f3c1df7331
Joe Watkins <info@joe-watkins.io>
2014-02-28 17:53:14 -0800

    Added (function(){})(); for Firefox compatibility

--- bc-id-grabber-bookmarklet.js
+++ bc-id-grabber-bookmarklet.js
@@ -1,17 +1,19 @@
-javascript: $("div.RadGrid_Default,div.RadTreeView_Default").each(function() {
-    $(this).find("a").each(function() {
-        var link = $(this).attr("href").split("="),
-        text = $(this).text(),
-        curURL = window.location.href;
-        if (curURL.indexOf("CustomContent_ItemList.aspx") != -1) {
-            $(this).text(text + " - " + link[2].replace("&ValueID", ""));
-        } else if (curURL.indexOf("WebForms.aspx") != -1) {
-            $(this).text(text + " - " + link[1]);
-        } else {
-            $(this).text(text + " - " + link[1].replace("&A", "")
-                .replace("&CustomContent", "")
-                .replace("WebFormID", "")
-                .replace("&ValueID", ""));
-        }
-    });
-});
+javascript:(function(){ 
+   $("div.RadGrid_Default,div.RadTreeView_Default").each(function() {
+        $(this).find("a").each(function() {
+            var link = $(this).attr("href").split("="),
+            text = $(this).text(),
+            curURL = window.location.href;
+            if (curURL.indexOf("CustomContent_ItemList.aspx") != -1) {
+                $(this).text(text + " - " + link[2].replace("&ValueID", ""));
+            } else if (curURL.indexOf("WebForms.aspx") != -1) {
+                $(this).text(text + " - " + link[1]);
+            } else {
+                $(this).text(text + " - " + link[1].replace("&A", "")
+                    .replace("&CustomContent", "")
+                    .replace("WebFormID", "")
+                    .replace("&ValueID", ""));
+            }
+        });
+    }); 
+})();
